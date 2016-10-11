3534875e9e88d65a1cebeab9666e37b2d26b4c53
gopiwrld@gmail.com <'gopiwrld@gmail.com'>
2016-10-11 19:37:22 +0530

    Was pending changes

--- src/main/java/com/deals/util/Core.java
+++ src/main/java/com/deals/util/Core.java
@@ -1,20 +1,26 @@
-/*package com.deals.util;
+package com.deals.util;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.tomcat.util.http.fileupload.FileUtils;
+import javax.crypto.Cipher;
 
 public class Core {
-
+	
+	 Cipher ecipher;
+	 Cipher dcipher;
+	 byte[] salt = {
+	        (byte) 0xA9, (byte) 0x9B, (byte) 0xC8, (byte) 0x32,
+	        (byte) 0x56, (byte) 0x35, (byte) 0xE3, (byte) 0x03
+	    };
+	 int iterationCount = 19;
+	
 	public static void main(String[] args){
-		String deletePath = "F:\\workspace\\practice\\BestDeals\\bestdeals\\src\\main\\resources\\static\\uploadimages\\A4uAsD_lord-hanuman-devotional-1920x978.jpg";
-
-		try {
-			FileUtils.forceDeleteOnExit(new File(deletePath));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		String val ="80874665as062";
+		String eKey = new Secret().encrypt(val);
+		System.out.println("Key ::: "+val);
+		System.out.println("E Key ::: "+eKey);
+		String dKey = new Secret().decrypt(eKey);
+		System.out.println("D Key ::: "+dKey);
 	}
+	
+	
+
 }
-*/
