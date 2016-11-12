9ca323658034219d861009e936d5040d14adcf8b
gsitgithub <gsitgithub@gmail.com>
2016-11-11 17:08:56 -0800

    LatLng info set using mouse click on location and red pointer

--- src/main/resources/static/scripts/services/u-profile.js
+++ src/main/resources/static/scripts/services/u-profile.js
@@ -110,14 +110,109 @@ function initMap() {
 	var lat = $('#latitude').val();
 	var lng = $('#longitude').val();
 	if(lat && lng){
-		var uluru = {lat: parseFloat(lat), lng: parseFloat(lng)};
+		/*var uluru = {lat: parseFloat(lat), lng: parseFloat(lng)};
 		var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 18,
 			center: uluru
 		});
-		var marker = new google.maps.Marker({
-			position: uluru,
-			map: map
-		});
+		*/
+	} else {
+	// Set defalt to "Pune"
+	    lat = 18.5204;
+	    lng = 73.8567;
 	}
-  }
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+      center: new google.maps.LatLng(lat, lng),
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP //'roadmap'
+    });
+    //google.maps.event.addListener(map,'click',function(event) {
+    map.addListener('click',function(event) {
+        //document.getElementById('latlongclicked').value = event.latLng.lat() + ', ' + event.latLng.lng();
+        $('#latitude').val(event.latLng.lat());
+        $('#longitude').val(event.latLng.lng());
+        placeMarker(event.latLng, map);
+        //map.setCenter(marker.getPosition());
+    });
+    //google.maps.event.addListener(map,'mousemove',function(event) { document.getElementById('latspan').innerHTML = event.latLng.lat() document.getElementById('lngspan').innerHTML = event.latLng.lng() document.getElementById('latlong').innerHTML = event.latLng.lat() + ', ' + event.latLng.lng() });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      if (marker) {
+          marker.setMap(null);
+      }
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+     infowindow = new google.maps.InfoWindow({content: "LatLng Applied" })
+}
+
+var marker;
+var infowindow;
+function placeMarker(location, map) {
+    if (marker) {
+        marker.setMap(null);
+    }
+    marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+    map.panTo(location);
+}
