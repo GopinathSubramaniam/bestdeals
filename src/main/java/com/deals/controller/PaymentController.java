2be2223600427bed9b72f264873b32cdd618f69a
gsitgithub <gsitgithub@gmail.com>
2016-12-27 23:52:45 +0530

    Fix

--- src/main/java/com/deals/controller/PaymentController.java
+++ src/main/java/com/deals/controller/PaymentController.java
@@ -5,18 +5,15 @@ import com.deals.model.User;
 import com.deals.service.PublicUserPlanService;
 import com.deals.service.UserService;
 import com.deals.service.payment.PaymentService;
-import com.deals.vo.PaymentVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sun.awt.ModalExclude;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
